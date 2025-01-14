9af5c6a738fcb4265316093c8f0fe807f30cd3c5
Gops-8 <gopalpadhi8@hotmail.com>
2025-01-15 00:15:58 +0530

    fixed ollama setting

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ APP_CONFIG = {
 import os
 
 OLLAMA_CONFIG = {
-    'BASE_URL': os.getenv('OLLAMA_URL', 'http://ollama-api:11434'),
+    'BASE_URL': os.getenv('OLLAMA_URL', 'http://localhost:11434'),
     'MODEL': os.getenv('OLLAMA_MODEL', 'llama3.1:8b'),
     'TEMPERATURE': 0.21
 }
